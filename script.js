74592d745f6542ec4df4e9102f987ef0c3f969c8
Mike Rivera <michaelstephenrivera@gmail.com>
2020-08-06 12:38:08 -0700

    Created logic for validating if fields are filled

---
+++ script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('passwordConfirm');
+
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+};
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+};
+
+// Check if email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (inputItem) {
+    if (inputItem.value.trim() === '') {
+      showError(inputItem, `${getFieldName(inputItem)} is required`);
+    } else {
+      showSuccess(inputItem);
+    }
+  });
+};
+
+// Get field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+// Event Listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, passwordConfirm]);
+});
